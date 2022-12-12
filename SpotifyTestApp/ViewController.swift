e3dd5472fa9a2fa39b46d0ec17afe9a5dededc03
Samantha Bennett <samm@sammantics.net>
2022-12-11 17:45:02 -0800

    Initial Commit

---
+++ SpotifyTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyTestApp
+//
+//  Created by Samantha Bennett on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
